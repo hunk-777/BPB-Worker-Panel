bbe14500f73bdc49ece78279cc5033b05c27c7e1
hunk-777 <149327155+hunk-777@users.noreply.github.com>
2024-10-04 19:48:22 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,13 +1,13 @@
 // @ts-nocheck
 // <!--GAMFC-->version base on commit 43fad05dcdae3b723c53c226f8181fc5bd47223e, time is 2023-06-22 15:20:02 UTC<!--GAMFC-END-->.
-// @ts-ignore
+// @ts-da0303
 // https://github.com/bia-pain-bache/BPB-Worker-Panel
 
 import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9';
+let userID = 'e354010b-3b26-4935-8081-d1308816aede';
 
 // https://www.nslookup.io/domains/bpb.yousef.isegaro.com/dns-records/
 const proxyIPs= ['bpb.yousef.isegaro.com'];
